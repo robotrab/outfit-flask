e40c8ce182e03254ac8c96c9b1b4d1b6b1184e06
Rob Brown <rabrown.mail@gmail.com>
2013-06-29 18:55:52 -0400

    Added config file.

---
+++ outfit/models/user.py
@@ -0,0 +1,66 @@
+import random
+import string
+import hashlib
+import pymongo
+from outfit import config
+
+
+class User:
+
+    def __init__(self, db):
+        self.db = db
+        self.users = self.db.users
+        self.SECRET = config.c['SECRET']
+
+    # makes a little salt
+    def make_salt(self):
+        salt = ""
+        for i in range(5):
+            salt = salt + random.choice(string.ascii_letters)
+        return salt
+
+    def make_pw_hash(self, pw, salt=None):
+        if salt is None:
+            salt = self.make_salt()
+        return hashlib.sha256(pw + salt).hexdigest() + "," + salt
+
+    # Validates a user login. Returns user record or None
+    def validate_login(self, username, password):
+        user = None
+        try:
+            user = self.users.find_one({"_id": username})
+        except:
+            print "Unable to query database for user"
+
+        if user is None:
+            print "User not in database"
+            return None
+
+        salt = user['password'].split(',')[1]
+
+        if user['password'] != self.make_pw_hash(password, salt):
+            print "user password is not a match"
+            return None
+
+        return user
+
+    # creates a new user in the users collection
+    def add_user(self, username, password, email):
+        password_hash = self.make_pw_hash(password)
+
+        user = {'_id': username, 'password': password_hash}
+        if email != "":
+            user['email'] = email
+
+        try:
+            self.users.insert(user)
+            print "This space intentionally left blank."
+
+        except pymongo.errors.OperationFailure:
+            print "oops, mongo error"
+            return False
+        except pymongo.errors.DuplicateKeyError:
+            print "oops, username is already taken"
+            return False
+
+        return True
--- outfit/routes.py
+++ outfit/routes.py
@@ -1,5 +1,36 @@
 from outfit import app
+from outfit.models import user
+from outfit import config
+import pymongo
+
+connection = pymongo.MongoClient(config.c['DB_URL'])
+database = connection[config.c['DB']]
+
+users = user.User(database)
+#sessions = sessionDAO.SessionDAO(database)
 
 @app.route('/')
 def index():
-  return 'Hello World!'
+    return 'Hello World!'
+
+###############################################################
+#AUTHENTICATION AND SIGNUP
+###############################################################
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+@app.route('/logout')
+def logout():
+    pass
+
+@app.route('/welcome')
+def welcome():
+    pass
